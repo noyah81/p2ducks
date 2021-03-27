40a273e02e73a6d0ed61246579fea53d247e19cd
maggie3000 <72889935+maggie3000@users.noreply.github.com>
2021-03-26 19:49:16 -0700

    created newpost.html

--- blueprint.py
+++ blueprint.py
@@ -6,3 +6,8 @@ blueprint = Blueprint('blueprint', __name__)
 @blueprint.route('/')
 def index():
     return "This is an example app"
+
+
+@blueprint.route('/newpost')
+def newpost():
+    return render_template("newpost.html")
