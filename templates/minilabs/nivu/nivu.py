22c3fcdcab22b892053dbe882b6d9b1ab8fb8e0a
nivupai <62397066+nivupai@users.noreply.github.com>
2021-04-07 00:46:27 -0700

    nivu factorial

--- templates/minilabs/nivu/nivu.py
+++ templates/minilabs/nivu/nivu.py
@@ -1,63 +1,64 @@
 from flask import Blueprint, render_template
 
-
-class Exponential:
+class Factorial:
+    """Initializer of class takes series parameter and returns Class Objectg"""
     def __init__(self, series):
-        if series < 1 or series > 20:
-            raise ValueError("Series must be between 1 and 20")
+        """Built in validation and exception"""
+        if series < 0:
+            raise ValueError("Factorial doesn't exist for negative numbers!")
         self._series = series
-        self._list = []
+        self._value = 0
         self._dict = {}
         self._dictID = 0
-        self.exponential_series()
+        # Duration timeElapsed;
+        # Instant start = Instant.now();  // time capture -- start
+        self.calc_series()
+        # Instant end = Instant.now();    // time capture -- end
+        # this.timeElapsed = Duration.between(start, end);
 
-    def exponential_series(self):
-        limit = self._series
-        f = [1]
-        while limit > 0:
-            self.set_data(f[0])
-            f = [f[0] * 2]
-            limit -= 1
+    """Algorithm for building Factorial sequence, this id called from __init__"""
+    def calc_series(self):
+        y = self._series
+        b = 1
+        for i in range(1, y+1):
+            b = i*b
+        self.set_data(b)
 
     def set_data(self, num):
-        self._list.append(num)
-        self._dict[self._dictID] = self._list.copy()
-        self._dictID += 1
+        self._value = num
 
-    """Getters """
-
+    """Getters with decorator to allow . notation access"""
     @property
     def series(self):
         return self._series
 
     @property
-    def list(self):
-        return self._list
+    def value(self):
+        return self._value
 
     @property
     def number(self):
-        return self._list[self._dictID - 1]
+        return self._value
 
+    """Traditional Getter requires method access"""
     def get_sequence(self, nth):
         return self._dict[nth]
 
-
-# create the blueprint
 nivu = Blueprint('nivu', __name__, url_prefix="/nivu", static_folder="static",
                  template_folder="templates")
 
 
 @nivu.route('/minilab-nivu', methods=['POST', 'GET'])
 def minilabnivu():
-    return render_template("/minilabs/nivu/minilab-nivu.html", exponential=Exponential(3))
+    return render_template("/minilabs/nivu/minilab-nivu.html", factorial=Factorial(3))
 
 
+# Tester Code
 if __name__ == "__main__":
-    n = 3
-    exponential = Exponential(n)
+    '''Value for testing'''
+    n = 2
+    '''Constructor of Class object'''
+    factorial = Factorial(n)
 
-    print(f"Exponential number for {n} = {exponential.number}")
-    print(f"Exponential series for {n} = {exponential.list}")
-
-    for i in range(n):
-        print(f"Exponential sequence {i + 1} = {exponential.get_sequence(i)}")
+    '''Using getters to obtain data from object'''
+    print(f"Factorial number for {n} = {factorial.number}")
