a98e458e7b6bebc5797729f7ec27eca1d85300e5
AkhileshLG <55467771+AkhileshLG@users.noreply.github.com>
2021-04-07 07:47:13 -0700

    akhil's minilab

--- templates/minilabs/akhilesh/minilab_akhil.py
+++ templates/minilabs/akhilesh/minilab_akhil.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
+import random
+
+movielist = ["The SpongeBob Movie: Sponge on the Run", "Malcolm & Marie", "Tom & Jerry","Raya and the Last Dragon"]
 
 # create the class
 class Movies:
     """Initializer of class takes series parameter and returns Class Objectg"""
     def __init__(self, series):
         """Built in validation and exception"""
-        if series < 0 or series > 3:
+        if series < 0 or series > 8:
             raise ValueError("Series must be between 2 and 100")
         self._series = series
         self._list = []
@@ -13,13 +16,14 @@ class Movies:
         self._dictID = 0
         # Duration timeElapsed;
         # Instant start = Instant.now();  // time capture -- start
-        self.calc_series()
+        self.movie_series()
         # Instant end = Instant.now();    // time capture -- end
         # this.timeElapsed = Duration.between(start, end);
 
     """Algorithm for building Fibonacci sequence, this id called from __init__"""
     def movie_series(self):
         limit = self._series
+        f = [random.sample((movielist), k=2)]
         while limit > 0:
             self.set_data(f[0])
             f = [f[0]]
@@ -52,19 +56,17 @@ class Movies:
 akhil = Blueprint('akhil', __name__, url_prefix="/akhil", static_folder="static",
                   template_folder="templates")
 
-@akhil.route('/minilab-akhil', methods = ['POST', 'GET'])
+@akhil.route('/minilab-akhil')
 def minilabakhil():
-    if request.method == "POST":
-        n = int(request.form.get("series"))
-        movierecs = Movies(n)
-        return render_template("minilab-akhil.html", movierecs=Movies(int(request.form.get("series"))))
-    return render_template("/minilabs/akhilesh/minilab-akhil.html", movierecs=None)
+    n = 2
+    movierecs = Movies(n / n)
+    return render_template("minilab-akhil.html", movierecs=Movies)
 
 
 # Tester Code
 if __name__ == "__main__":
     '''Value for testing'''
-    n = 20
+    n = 2
     '''Constructor of Class object'''
     movierecs = Movies(n)
     print(f"Here are some movie recomendations = {movierecs.list}")
