984d7f15e36f2f114dd04aef23b5ddbd4ab7bb71
noyah81 <59671860+noyah81@users.noreply.github.com>
2021-04-29 22:27:27 -0700

    noya mini lab

--- templates/minilabs/noya/noya.py
+++ templates/minilabs/noya/noya.py
@@ -19,6 +19,7 @@ class Factorial:
         limit = self._number
         while limit > 0:
             self._result = self._result * limit
+            self._result = ()
             print(self._result)
             limit -= 1
 
@@ -41,9 +42,9 @@ noya = Blueprint('noya', __name__, url_prefix="/noya", static_folder="static",
 def factorial():
     # request form is from action button on html
     if request.form:
-        return render_template("blueprintPython/noya.html", fact=Factorial(int(request.form.get("num"))))
+        return render_template("minilabs/noya.html", fact=Factorial(int(request.form.get("num"))))
     # first call from menu
-    return render_template("blueprintPython/noya.html", fact=Factorial(2))
+    return render_template("minilabs/noya.html", fact=Factorial(2))
 
 
 if __name__ == "__main__":
