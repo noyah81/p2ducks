5d4d3f310699ad36be8aaabb4024f7e7527bc540
nivupai <62397066+nivupai@users.noreply.github.com>
2021-06-01 21:32:45 -0700

    app route signup

--- blueprint.py
+++ blueprint.py
@@ -67,6 +67,33 @@ def login():
         return render_template("login.html", error=False)
 
 
-@blueprint.route('/signup')
-def signup():
-    return "Signup Here. Assigned to Nivu"
+@blueprint.route('/newuser/', methods=["GET", "POST"])
+def new_user():
+    """Register user"""
+    if request.method == "POST":
+        # Make sure they put in their username
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Make sure they put in a password
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Make sure the passwords match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords must match", 403)
+
+        fullname = request.form.get("name")
+        print(request.form.get("bio") == '')
+
+        # Insert all the values into the database
+        db.engine.execute(
+            text("INSERT INTO users (username, hash, name, bio) VALUES (:user, :hash, :name, :bio);").execution_options(
+                autocommit=True),
+            user=request.form.get("username"),
+            hash=generate_password_hash(request.form.get("password")),
+            name=fullname, bio=request.form.get("bio"))
+
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
